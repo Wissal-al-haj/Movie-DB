7c91e4c3019aed17af8265cb8ab23f59e628a120
wissal al haj <wissalalhj@gmail.com>
2021-12-25 19:34:25 +0200

    step 3

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ app.get('/', (req, res) => {
     res.send('ok')
  
 })
+app.get('/test', (req, res) => {
+    res.send({status:200, message:"ok"})
+ 
+})
+app.get('/time', (req, res) => {
+    const nDate = new Date().toLocaleString('en-US', { hour: 'numeric', hour12: true ,minute:'numeric',minute60:true});
+    res.send({status:200, message:nDate})
+})
+
 app.listen(port, () => console.log(`the server started at http://localhost:${port}`))
