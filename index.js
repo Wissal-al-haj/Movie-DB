06eab441b39d624a41b07ebb06e9cdf6cd5863ab
wissal al haj <wissalalhj@gmail.com>
2021-12-28 14:37:12 +0200

    step 5

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.json())
-/**step2 */
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+/**step 2 */
 app.get('/', (req, res) => {
     res.send('ok')
  
 })
-/**step3 */
+
+/**step 3 */
 app.get('/test', (req, res) => {
     res.send({status:200, message:"ok"})
  
@@ -16,7 +23,8 @@ app.get('/time', (req, res) => {
     const nDate = new Date().toLocaleString('en-US', { hour: 'numeric', hour12: true ,minute:'numeric',minute60:true});
     res.send({status:200, message:nDate})
 })
-/**step4 */
+
+/**step 4 */
 app.get('//hello/:id', (req, res) => {
     req.params.id
     res.send({status:200, message:`Hello ${req.params.id}`})
@@ -30,4 +38,25 @@ app.get('/search',(req, res) => {
     res.send({status:500, error:true, message:"you have to provide a search"})
  })
 
+ /**step 5 */
+app.get('/movies/create', (req, res) => {
+    res.send({status:200, message:"ok"})
+ 
+})
+   
+app.get("/movies/read", (req, res) => {
+res.send({status: 200,data: movies})
+})
+
+app.get('/movies/update', (req, res) => {
+    res.send({status:200, message:"ok"})
+ 
+})
+app.get('/movies/delete', (req, res) => {
+    res.send({status:200, message:"ok"})
+ 
+})
+
 app.listen(port, () => console.log(`the server started at http://localhost:${port}`))
+
+
